e1336e9fd57c11f20c3a52a3617b5b7d6b20ce17
CYC666 <13705038428@163.com>
2018-06-27 10:35:36 +0800

    11

---
+++ Myself/Myself/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Myself
+//
+//  Created by 曹老师 on 2018/6/27.
+//  Copyright © 2018年 曹奕程. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Myself/Myself/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Myself
+//
+//  Created by 曹老师 on 2018/6/27.
+//  Copyright © 2018年 曹奕程. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
